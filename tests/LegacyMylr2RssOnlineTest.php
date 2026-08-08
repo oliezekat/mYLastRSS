227491a56ca4d45111247ecaea8c7943b5345a53
oliezekat <oliezekat@yahoo.fr>
2026-08-08 23:26:02 +0200

    #4 add LegacyMylr2RssOnlineTest
    limit checking to 1 item per source

---
+++ tests/LegacyMylr2RssOnlineTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Oliezekat\MyLastRss\Tests;
+
+final class LegacyMylr2RssOnlineTest extends AbstractMylr2RssTestCase
+{
+    use SourcesProvidersTrait;
+
+    public static function sourcesProvider()
+    {
+        return self::urlsSourcesProvider(1);
+    }
+
+}
--- tests/SourcesProvidersTrait.php
+++ tests/SourcesProvidersTrait.php
@@ -67,7 +67,7 @@ trait SourcesProvidersTrait
         ];
     }
 
-    public static function urlsSourcesProvider()
+    public static function urlsSourcesProvider($minItemsPerSource = null)
     {
         $urls = self::urlSourceProvider();
         $nbUrls = count($urls);
@@ -75,7 +75,7 @@ trait SourcesProvidersTrait
         $sources = [];
         foreach ($urls as $url) {
             $sources[] = $url[0];
-            $nbItems += $url[1];
+            $nbItems += ($minItemsPerSource === null ? $url[1] : intval($minItemsPerSource, 10));
         }
         $data = [
             $nbUrls . ' URLs' => [
