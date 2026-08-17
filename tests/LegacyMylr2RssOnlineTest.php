efe1aea6e3d2f6d5c498d334586f7074e6d42c1f
oliezekat <oliezekat@yahoo.fr>
2026-08-18 00:03:25 +0200

    #27 fix testGetOutputSaved

--- tests/LegacyMylr2RssOnlineTest.php
+++ tests/LegacyMylr2RssOnlineTest.php
@@ -17,7 +17,7 @@ final class LegacyMylr2RssOnlineTest extends AbstractMylr2RssTestCase
         $instance                            = parent::createClassInstance();
         $instance->cache_feeds_if_failed     = true;
         $instance->max_execution_time        = 50;
-        $instance->kidx_rule                 = 'date+title';
+        $instance->kidx_rule                 = 'date+title/link';
         return $instance;
     }
 
